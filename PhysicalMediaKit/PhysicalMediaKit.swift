8d37a5ebd82f2a9c9b53d90ab2a4024d07fa3ba7
Spencer Hartland <spencerhartland@icloud.com>
2025-06-12 16:32:30 -0700

    Added `compactDisc` and `compactCassette` properties to `PhysicalMedia`.

--- PhysicalMediaKit/PhysicalMediaKit.swift
+++ PhysicalMediaKit/PhysicalMediaKit.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+/// Provides methods to render customizable, animated 3D models of common physical media (vinyl record, CD, etc.).
 public struct PhysicalMedia {
-    // TODO: Convert to function that accepts albumArt (url) and vinylColor as parameters.
+    // TODO: Convert to function that accepts albumArt and vinylColor as parameters.
     /// A view displaying an animated 3D model of a vinyl record partially pulled from its sleeve.
     public static var vinylRecord: some View { VinylRecord3DModelView() }
+    
+    // TODO: Convert to function that accepts albumArt as a parameter.
+    /// A view displaying an animated 3D model of a compact disc inside of its partially open case.
+    public static var compactDisc: some View { CompactDisc3DModelView() }
+    
+    // TODO: Convert to function that accepts albumArt and cassetteColor as parameters.
+    /// A view displaying an animated 3D model of a compact cassette inside of its case.
+    public static var compactCassette: some View { CompactCassette3DModelView() }
 }
